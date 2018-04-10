2599a2e421979deb2be0e58aa22e095cd03cb713
Matt Tripodi <mtripodi1027@gmail.com>
2018-04-10 16:08:40 -0400

    Initial Commit

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by Matt Tripodi on 4/10/18.
+//  Copyright © 2018 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
